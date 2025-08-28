49acecd46d99e8f1b45709a450aa14d3814df83d
tar1Qul <tariqul.soykot.001.com>
2025-08-28 09:42:08 +0600

    add function for heart icon

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
 function getElement(id) {
     return (element = document.getElementById(id));
 }
+
+getElement("card-container").addEventListener("click", function (e) {
+    if (e.target.className.includes("cart-love")) {
+        const hartCount = getElement("hart-count").innerText;
+        const currentHartCount = Number(hartCount) + 1;
+        getElement("hart-count").innerText = currentHartCount;
+    }
+});
